7dc5320993c812c61abca3c5501594a7522e0681
fahimlisl <developerfahim134@gmail.com>
2025-12-07 01:03:41 +0530

    starting work on review section of backend

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+// import express from "express";
+
+// const app = express()
+
+// app.listen(7000 , (req,res) => {
+//     console.log(`app is listening on port 7000`)
+// })
---
+++ backend/src/index.js
@@ -0,0 +1,6 @@
+import { app } from "./app.js";
+
+
+app.listen(7000 , (req,res) => {
+    console.log(`exmaple app is listneing on 7000`)
+})
