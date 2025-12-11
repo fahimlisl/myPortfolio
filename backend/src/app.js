61831e030d1cceda299a08436248411cdd98a587
fahimlisl <developerfahim134@gmail.com>
2025-12-11 16:53:38 +0530

    added antoher dotenv

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,14 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import dotenv from "dotenv"
+
 
 const app = express();
 
+dotenv.config({
+  path:"./.env"
+})
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
