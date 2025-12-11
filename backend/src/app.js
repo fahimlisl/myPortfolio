7db451fb3212dfab7db86d006a7e992ae746dd84
fahimlisl <developerfahim134@gmail.com>
2025-12-11 17:01:13 +0530

    ...

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,20 +2,12 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import dotenv from "dotenv"
-
 
 const app = express();
 
-dotenv.config({
-  path:"./.env"
-})
-
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 
